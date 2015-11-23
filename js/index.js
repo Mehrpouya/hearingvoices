0269e989dcd2a064d3e619f29556d9392a29c4e9
HM <loki@c119181.wlan.net.ed.ac.uk>
2015-11-23 18:47:53 +0000

    parallax background, more work on intro and survey, also pdf donwload

--- js/index.js
+++ js/index.js
@@ -30,3 +30,23 @@ function navClicked(_whereTo){
         scrollTop: $(_whereTo).offset().top-50
     }, 2000);
 }
+
+
+$(document).ready(function(){
+  $('#survey img').bind("click",function(){
+    $('#survey').fadeOut("slow");
+  });
+    $('section[data-type="background"]').each(function(){
+        var $bgobj = $(this); // assigning the object
+
+        $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        });
+    });
+});
